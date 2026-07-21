d5eaf98b74e3061b5539680a83ef6f9bc1f69e8e
smazydev <alizafar1437@gmail.comm>
2026-07-21 16:12:36 +0500

    Clean repo - remove all project files

--- test-db.ts
+++
@@ -1,5 +0,0 @@
-import { prisma } from './src/lib/prisma';
-prisma.campaign.findMany().then(res => console.log('SUCCESS', res)).catch(e => {
-  console.error('QUERY ERROR', e);
-  console.error(e.stack);
-});
